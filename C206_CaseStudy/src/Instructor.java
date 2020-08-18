5f6fa051c51d520478aedec6b51ebfa2c73e9d16
19018247 <19018247@DILLAH-LAPTOP>
2020-08-18 17:23:18 +0800

    update viewAllChromebood() in ResourcCentre

---
+++ C206_CaseStudy/src/Instructor.java
@@ -0,0 +1,32 @@
+
+public class Instructor {
+	
+	private String name;
+	private String CCA;
+
+	public Instructor(String name, String CCA) {
+		this.name = name;
+		this.CCA = CCA;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCCA() {
+		return CCA;
+	}
+
+	public void setCCA(String CCA) {
+		this.CCA = CCA;
+	}
+	
+	
+	
+
+}
