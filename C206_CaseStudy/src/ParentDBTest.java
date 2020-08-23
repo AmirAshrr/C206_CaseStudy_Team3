a0752da725e8673f3d896d1d224ddb2dcc881447
OngJunJieRP <19032110@myrp.edu.sg>
2020-08-23 18:17:34 +0800

    Updated codes to meet the new requirements

--- C206_CaseStudy/src/ParentDBTest.java
+++ C206_CaseStudy/src/ParentDBTest.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ParentDBTest {
 	
@@ -25,4 +26,85 @@ public class ParentDBTest {
 		}
 		return output;
 	}
+
+	public static String checkEmail() {
+		String output = "";
+		for (int i = 0;i < parentList.size(); i++) {
+			if (!parentList.get(i).getEmail().contains("@")) {
+				output = "Email does not contain \"@\" symbol";
+			}
+			else {
+				output = "Email contain \"@\" symbol";
+			}
+		}
+		return output;
+	}
+
+	public static String updateParent(String name, String email, String address, int number) {
+		String output = "";
+		for (int i = 0; i < parentList.size(); i++) {
+			if (parentList.get(i).getName().equals(name)) {
+				parentList.get(i).setEmail(email);
+				parentList.get(i).setAddress(address);
+				parentList.get(i).setContact_num(number);
+			}
+			else {
+				System.out.println("Parent details cannot be found");
+			}
+		}
+		return output;
+	}
+
+	public static String parentGenerateID(String email) {
+		String registration_id = "";
+		String output = "";
+		//int max_num = 9;
+		Random rand = new Random();
+		for (int i = 0; i < parentList.size(); i++) {
+			if (parentList.get(i).getEmail().equalsIgnoreCase(email)) {
+				for (int num = 0; num <5; num++) {
+					// Obtain a number from  0 - 9
+					int random_num = rand.nextInt(10);
+					registration_id += "" + random_num;
+					//registration_id += random_num;
+				}
+				int registrationID = Integer.parseInt(registration_id) ;
+				output += "Registration ID has been assigned!";
+				parentList.get(i).setRegistration_ID(registrationID);
+
+			}
+			else if (!parentList.get(i).getEmail().equalsIgnoreCase(email)){
+				output += "Email cannot be found!";
+			}
+		}
+		return output;
+	}
+
+	public static String parentLogIn(String email, int registration_id, int studentId, String studentName) {
+		String output = "";
+			for (int i = 0; i <parentList.size(); i++) {
+				if (parentList.get(i).getEmail().equalsIgnoreCase(email) && parentList.get(i).getRegistration_ID() == registration_id) {
+					//output += "Log in successful!";
+					for (int a = 0; a< StudentDB.studentList.size(); a++) {
+						if (StudentDB.studentList.get(a).getId() == studentId && StudentDB.studentList.get(a).getName().equalsIgnoreCase(studentName)) {
+							output += "Log in successful!";
+						}
+					}
+				}
+			}
+		
+		return output;
+	}
+
+	public static String registerStudent(int studentID, String ccaName) {
+		String output = "";
+		for (int i = 0; i < StudentDB.studentList.size(); i ++) {
+			if (StudentDB.studentList.get(i).getId() == studentID) {
+				output += "Student has successfully registered!";
+			}
+		}
+		return output;
+	}
+	
+	
 }
--- C206_CaseStudy/test/C206_ParentTest.java
+++ C206_CaseStudy/test/C206_ParentTest.java
@@ -10,14 +10,17 @@ public class C206_ParentTest {
 
 	private ParentTest parent1;
 	private ParentTest parent2;
+	private Student student1;
 	
 	@Before
 	public void setUp() throws Exception {
 		
-		parent1 = new ParentTest("Adam" , "Adam@gmail.com",
-				"blk 111 street 00", 88888888);
+		parent1 = new ParentTest("Adam" , "Adamgmail.com",
+				"blk 111 street 00", 88888888, 00000);
 		parent2 = new ParentTest("Sally", "Sally@gmail.com",
-				"blk 222 street 11", 87777777);
+				"blk 222 street 11", 87777777, 00001);
+		
+		student1 = new Student(12345, "JunJie", 3, "4A", "Mr Ong");
 		
 	}
 
@@ -25,7 +28,9 @@ public class C206_ParentTest {
 	public void tearDown() throws Exception {
 		parent1 = null;
 		parent2 = null;
+		student1 = null;
 		ParentDBTest.parentList.clear();
+		StudentDB.studentList.clear();
 		
 	}
 
@@ -37,7 +42,7 @@ public class C206_ParentTest {
 		// Test the size of arrayList is 0 before adding any parent
 		assertEquals("Test the size of arrayList is 0 before adding any parent: " , 0 , ParentDBTest.parentList.size());
 		
-		// Test the size of arraylist is 1 after adding 1 parent  
+		// Test the size of arraylist is 1 after adding 1 parent   
 		ParentDBTest.addParent(parent1);
 		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
 		
@@ -75,4 +80,101 @@ public class C206_ParentTest {
 		assertEquals("Test if an appropriate message is displayed when arrayList size is 0" ,String.format("%-10s %-20s %-20s %-10d", ParentDBTest.parentList.get(0).getName(), ParentDBTest.parentList.get(0).getEmail(), ParentDBTest.parentList.get(0).getAddress(), ParentDBTest.parentList.get(0).getContact_num()), ParentDBTest.viewAllParents());
 	
 	}
+	
+	@Test
+	public void doCheckEmail() {
+		// Test if arrayList is not null
+		assertNotNull("Test if the arrayList is not null: ", ParentDBTest.parentList);
+		
+		// Test if the arrayList size is 0
+		assertEquals("Test if the arrayList size is 0: ", 0 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when arrayList size is 1		
+		ParentDBTest.addParent(parent1);
+		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when the email does not contains "@" symbol
+		assertEquals("Test if an appropriate message is displayed when the email does not contains \"@\" symbol", "Email does not contain \"@\" symbol", ParentDBTest.checkEmail());
+		ParentDBTest.removeParent(parent1);
+		
+		// Test if an appropriate message is displayed when the email contains "@" symbol
+		ParentDBTest.addParent(parent2);
+		assertEquals("Test if an appropriate message is displayed when the email does not contains \"@\" symbol", "Email contain \"@\" symbol", ParentDBTest.checkEmail());
+
+	}
+	
+	@Test
+	public void updateParent() {
+		// Test if arrayList is not null
+		assertNotNull("Test if the arrayList is not null: ", ParentDBTest.parentList);
+		
+		// Test if the arrayList size is 0
+		assertEquals("Test if the arrayList size is 0: ", 0 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when arrayList size is 1		
+		ParentDBTest.addParent(parent1);
+		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
+		
+		// Test if parent email, address and mobile_num is changed to Adam@gmail.com, blk 111 street 01, 88888887
+		ParentDBTest.updateParent("Adam", "Adam@gmail.com", "blk 111 street 01", 88888887);
+		assertEquals("Test if parent email is changed to Adam@gmai.com", "Adam@gmail.com", ParentDBTest.parentList.get(0).getEmail());
+		assertEquals("Test if parent address is changed to blk 111 street 01", "blk 111 street 01", ParentDBTest.parentList.get(0).getAddress());
+		assertEquals("Test if parent mobile_num is changed to 88888887", 88888887, ParentDBTest.parentList.get(0).getContact_num());
+	}
+	
+	@Test 
+	public void parentRegistrationID() {
+		// Test if arrayList is not null
+		assertNotNull("Test if the arrayList is not null: ", ParentDBTest.parentList);
+		
+		// Test if the arrayList size is 0
+		assertEquals("Test if the arrayList size is 0: ", 0 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when arrayList size is 1		
+		ParentDBTest.addParent(parent2);
+		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
+		
+		// Test if an appropriate message is displayed when the email is found 
+		assertEquals("Test if an appropirate message is displayed when the registration ID matches", "Registration ID has been assigned!", ParentDBTest.parentGenerateID("Sally@gmail.com"));	
+	
+		// Test if an appropriate message is displayed email is not found
+		ParentDBTest.removeParent(parent2);
+		ParentDBTest.addParent(parent1);
+		assertEquals("Test if an appropirate message is displayed when the registration ID matches", "Email cannot be found!", ParentDBTest.parentGenerateID("Adam@gmail.com"));	
+	}
+	
+	@Test 
+	public void parentLogIn() {
+		// Test if arrayList is not null
+		assertNotNull("Test if the arrayList is not null: ", ParentDBTest.parentList);
+		
+		// Test if the arrayList size is 0
+		assertEquals("Test if the arrayList size is 0: ", 0 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when arrayList size is 1		
+		ParentDBTest.addParent(parent2);
+		StudentDB.addStudent(student1);
+		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
+		
+		// Test if an appropriate message is displayed when successfully logged in
+		assertEquals("Test if an appropriate message is displayed when successfully logged in", "Log in successful!", ParentDBTest.parentLogIn("Sally@gmail.com", 00001, 12345, "JunJie"));
+	}
+	
+	@Test
+	public void registerStudentForCCA() {
+		// Test if arrayList is not null
+		assertNotNull("Test if the arrayList is not null: ", ParentDBTest.parentList);
+		
+		// Test if the arrayList size is 0
+		assertEquals("Test if the arrayList size is 0: ", 0 , ParentDBTest.parentList.size());
+
+		// Test if an appropriate message is displayed when arrayList size is 1		
+		ParentDBTest.addParent(parent2);
+		StudentDB.addStudent(student1);
+		assertEquals("Test the size of arraylist is 1 after adding 1 parent: ", 1 , ParentDBTest.parentList.size());
+		
+		// Test if an appropriate message is displayed when a student successfully joined a CCA
+		assertEquals("Test if an appropriate message is displayed when a student successfully joined a CCA", "Student has successfully registered!", ParentDBTest.registerStudent(12345, "Basketball"));
+	}
+	
 }
