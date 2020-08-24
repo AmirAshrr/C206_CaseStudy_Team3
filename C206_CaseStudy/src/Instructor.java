0129e9475b7a8dea34fe4766108cb1939c5f2431
OngJunJieRP <19032110@myrp.edu.sg>
2020-08-24 22:13:19 +0800

    updated constructor

--- C206_CaseStudy/src/Instructor.java
+++ C206_CaseStudy/src/Instructor.java
@@ -7,7 +7,9 @@ public class Instructor {
 
 
 	public Instructor(String name, String email, String password) {
-
+		this.name = name;
+		this.email = email;
+		this.password = password;
 	}
 
 	public String getName() {
